de4e05e7397854179d10e18f7883cb6fee044432
Evgeniy Zaharov <rocketjump07@gmail.com>
2013-10-15 18:52:17 +0700

    PACANI UJE BLIZKO IDLE3

--- task2.py
+++ task2.py
@@ -2,20 +2,33 @@ import random
 
 def main () :
     def register(name) :
-        fp = open (base,'+')        
-        for line in fp :            
-            if name == line :
-                print("Такое имя уже есть")
-                t = t + random.choice()
-                fp.close()
-                fp = open ()
-            else :
-                fp.write(name)
-    print("Введите имя")        
+        fp = open ('/home/student/python_specialKurs/testing.txt','r')     
+        l = open('/home/student/python_specialKurs/testing.txt').readlines()
+        fp.close
+        if name in l :                
+            print("Такое имя уже есть")
+            name1 = name + random.choice()
+            name2 = name + random.choice()
+            name3 = name + random.choice()
+            name4 = name + random.choice()
+            print("Возможные варианты регистрации :",name1,'\n',name2,'\n',
+                      name3,'\n',name4)            
+            try :
+                nameuse = str(input("Введите одно из этих похожих имен, или новое имя"))
+            except TypeError:
+                print("неккоректный ввод")          
+            fp = open ('/home/student/python_specialKurs/testing.txt','a')
+            fp.write(nameuse + '\n')
+            fp.close()
+        else :
+            fp = open ('/home/student/python_specialKurs/testing.txt','a')
+            fp.write(name + '\n')
+            fp.close()          
     try :
-        name = string (input())
+        name = str(input("Введите имя"))
     except TypeError:
         print("неккоректный ввод")
+  
     else :
          register(name)   
    
