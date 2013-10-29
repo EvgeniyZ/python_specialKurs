5a09715fe7b2d8c3e574099b50c90288e0dad998
Evgeniy Zaharov <rocketjump07@gmail.com>
2013-10-29 18:52:15 +0700

    almost pep8

--- task2.py
+++ task2.py
@@ -1,40 +1,50 @@
 import random
 import os
-def main () :
-    def register(name) :
+def main():
+    def register(name):
         namefile = input("Введите имя файла")
-        fp = open ('/home/student/python_specialKurs/'+namefile,'r+')        
-        for line in fp :                                
-            if (name==line.rstrip()) :
-                print("Такое имя уже есть")
-                name1 = name + str(random.randint(0,100000))
-                name2 = name + str(random.randint(0,100000))
-                name3 = name + str(random.randint(0,100000))
-                name4 = name + str(random.randint(0,100000))
-                print("Возможные варианты регистрации :",name1,'\n',name2,'\n',
-                      name3,'\n',name4)            
-                try :
-                    nameuse = str(input("Введите одно из этих похожих имен, или новое имя"))
-                except TypeError:
-                    print("неккоректный ввод")
-                fp.close()
-                fp = open ('/home/student/python_specialKurs/testing.txt','w')
-                fp.write(nameuse + '\n')
-                fp.close()
-                break
-            else :               
+        if os.path.getsize('/home/student/python_specialKurs/'
+                           + namefile) == 0:
+            print("пишем в пустой файл!")
+            fp = open('/home/student/python_specialKurs/' + namefile, 'w')
+            fp.write(name + '\n')
+            fp.close()
+        else:
+            fp = open('/home/student/python_specialKurs/' + namefile, 'r')
+            for line in fp:
+                if (name == line.rstrip()):
+                    print("Такое имя уже есть")
+                    name1 = name + str(random.randint(0, 100000))
+                    name2 = name + str(random.randint(0, 100000))
+                    name3 = name + str(random.randint(0, 100000))
+                    name4 = name + str(random.randint(0, 100000))
+                    print("Возможные варианты регистрации: ", name1,
+                          '\n', name2, '\n', name3, '\n', name4)
+                    try:
+                        nameuse =
+                        str(input
+                            ("Введите одно из этих похожих имен"))
+                    except TypeError:
+                        print("неккоректный ввод")
+                    fp.close()
+                    fp = open
+                    ('/home/student/python_specialKurs/' + namefile, 'w')
+                    fp.write(nameuse + '\n')
+                    fp.close()
+                    break
+            else:
                 print("вы ввели удачное имя")
                 fp.close()
-                fp = open ('/home/student/python_specialKurs/testing.txt','w')
+                fp = open('/home/student/python_specialKurs/' + namefile, 'w')
                 fp.write(name + '\n')
-                fp.close()         
-    try :
-        name = input("Введите имя")      
+                fp.close()
+    try:
+        name = input("Введите имя")
     except TypeError:
         print("неккоректный ввод")
   
-    else :
-         register(name)   
+    else:
+        register(name)   
    
 if __name__=='__main__' :
     main ()
