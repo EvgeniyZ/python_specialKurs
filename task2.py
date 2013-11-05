07a3cfc03e5da4333e34b6360dbed679faf725f5
Evgeniy Zaharov <rocketjump07@gmail.com>
2013-11-05 18:54:38 +0700

    vse chetko po pep8

--- task2.py
+++ task2.py
@@ -1,17 +1,13 @@
 import random
 import os
+
+
 def main():
-    def register(name):
-        namefile = input("Введите имя файла")
-        if os.path.getsize('/home/student/python_specialKurs/'
-                           + namefile) == 0:
-            print("пишем в пустой файл!")
-            fp = open('/home/student/python_specialKurs/' + namefile, 'w')
-            fp.write(name + '\n')
-            fp.close()
+    def watch(l, name, namefile):
+        if not l:
+            placein(name, namefile)
         else:
-            fp = open('/home/student/python_specialKurs/' + namefile, 'r')
-            for line in fp:
+            for line in l:
                 if (name == line.rstrip()):
                     print("Такое имя уже есть")
                     name1 = name + str(random.randint(0, 100000))
@@ -20,31 +16,39 @@ def main():
                     name4 = name + str(random.randint(0, 100000))
                     print("Возможные варианты регистрации: ", name1,
                           '\n', name2, '\n', name3, '\n', name4)
-                    try:
-                        nameuse =
-                        str(input
-                            ("Введите одно из этих похожих имен"))
-                    except TypeError:
-                        print("неккоректный ввод")
-                    fp.close()
-                    fp = open
-                    ('/home/student/python_specialKurs/' + namefile, 'w')
-                    fp.write(nameuse + '\n')
-                    fp.close()
-                    break
+                    nameuse = input("Введите одно из этих похожих имен")
+                    watchout(l, nameuse, namefile)
+
+    def watchout(l, nameuse, namefile):
+        for line in l:
+            if (nameuse == line.rstrip()):
+                print("Вы повторили неправильный ввод, попробуйте еще раз")
+                name = input("Введите имя")
+                watchout(l, name, namefile)
             else:
-                print("вы ввели удачное имя")
-                fp.close()
-                fp = open('/home/student/python_specialKurs/' + namefile, 'w')
-                fp.write(name + '\n')
-                fp.close()
-    try:
-        name = input("Введите имя")
-    except TypeError:
-        print("неккоректный ввод")
-  
-    else:
-        register(name)   
-   
-if __name__=='__main__' :
-    main ()
+                placein(nameuse, namefile)
+
+    def placein(nameuse, namefile):
+        print("вы ввели удачное имя")
+        fp = open('/home/student/python_specialKurs/' + namefile,
+                  'a+')
+        fp.write(nameuse + '\n')
+        fp.close()
+
+    def register(name):
+        namefile = input("Введите имя файла")
+        if os.path.exists('/home/student/python_specialKurs/' + namefile):
+            fp = open('/home/student/python_specialKurs/' + namefile)
+            l = [line.strip() for line in fp]
+            else:
+                fp = open('/home/student/python_specialKurs/' + namefile,
+                          'x')
+                l = []
+        fp.close()
+        watch(l, name, namefile)
+    name = input("Введите имя")
+    register(name)
+
+
+if __name__ == '__main__':
+    main()
